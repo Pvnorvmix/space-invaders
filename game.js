2f26c2dcb903c4f7475ddbed5292b7b60386bbb7
Pvnorvmix <m.marcelpro@gmail.com>
2024-09-27 16:33:02 +0200

    Update game.js

--- game.js
+++ game.js
@@ -1,1 +1,19 @@
+const canvas = document.getElementById('gameCanvas');
+const ctx = canvas.getContext('2d');
+
+canvas.width = 800;
+canvas.height = 600;
+
+function draw() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.fillStyle = 'white';
+  ctx.fillRect(100, 100, 50, 50);  // Just a placeholder for your spaceship
+}
+
+function gameLoop() {
+  draw();
+  requestAnimationFrame(gameLoop);
+}
+
+gameLoop();
 
